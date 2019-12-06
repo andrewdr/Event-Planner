5f7890dfe575247477e7c14f5172538e3f80ae78
Andrew Draper <andy.draper83@gmail.com>
2019-12-05 23:29:38 -0600

    Added appropriate time and date fields

---
+++ Event-Planner/CreateEventController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateEventController.swift
+//  Event-Planner
+//
+//  Created by The Clout on 12/5/19.
+//  Copyright © 2019 Draper Web Services. All rights reserved.
+//
+
+import UIKit
+
+class CreateEventController: UIViewController {
+    
+    @IBOutlet weak var eventName: UITextField!
+    @IBOutlet weak var eventDescription: UITextField!
+    @IBOutlet weak var eventTime: UITextField!
+    @IBOutlet weak var eventDate: UITextField!
+    @IBOutlet weak var eventAddress: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
